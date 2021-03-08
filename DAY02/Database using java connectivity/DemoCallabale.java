16f74e086755b372afc43e0e7401bafdb72b5544
rohini-kesireddy <78852450+rohini-kesireddy@users.noreply.github.com>
2021-03-08 19:21:52 +0530

    Create DemoCallabale.java

---
+++ DAY02/Database using java connectivity/DemoCallabale.java
@@ -0,0 +1,24 @@
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DemoCallable {
+
+	public static void main(String[] args) throws SQLException {
+		// TODO Auto-generated method stub
+		Connection c=DriverManager.getConnection("jdbc:mysql://localhost:3306/roshan","root","root");
+		CallableStatement cs=c.prepareCall("{call getAllStudents}");
+		ResultSet rs=cs.executeQuery();
+		while(rs.next())
+		{
+			System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getString(3));
+		}
+		c.close();
+		
+
+	}
+
+}
